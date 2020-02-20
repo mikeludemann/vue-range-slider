583705a825b5a719031a9576059a28de0fd5eb98
Mike Ludemann <mike-ludemann@hotmail.de>
2020-02-20 16:12:11 +0100

    Setup complete - vue-range-slider

---
+++ src/components/range-slider.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+
+	<div class="range-slider">
+		<input type="range" :id="id" :name="name" :min="min" :step="step" :max="max" @input="changeHandling" @change="changeHandling">
+	</div>
+
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'rangeSlider',
+		props: {
+			id: String,
+      name: String,
+      min: String,
+      max: String,
+      step: String
+		},
+		mounted () {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+			changeHandling ({ type, target }) {
+				const event = {
+					type,
+					isCheckbox: target.type === 'checkbox',
+					target: {
+						value: target.value,
+						checked: target.checked
+					}
+				}
+				console.log(`${event.type}: ${event.isCheckbox ? event.target.checked : event.target.value}`);
+			}
+		},
+		computed: {
+
+		}
+}
+
+
+</script>
+
+<style scoped lang="scss">
+	.range-slider {
+
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <rangeSlider min="1" max="1000" step="10" name="test" id="test1"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import rangeSlider from '@/components/range-slider.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    rangeSlider
+  }
+}
+</script>
